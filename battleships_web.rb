030b384a0a539369b4adda4e6a492d60aad145e8
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-16 17:17:45 +0100

    damn thing remembers names at last

--- battleships_web.rb
+++ battleships_web.rb
@@ -8,17 +8,22 @@ require_relative 'lib/cell'
 require_relative 'lib/board'
 
 class BattleshipsWeb < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   get '/new_game' do
-    @name = params[:name]
     erb :new_game
   end
 
   get '/board' do
-    @name = params[:name]
+    p params
+    p params[:name]
+    p session
+    session[:name] = params[:name] unless (params[:name] == '' || params[:name] == nil)
+    @name = session[:name] #if session[:name] != ''
     board1 = Board.new(Cell)
     player1 = Player.new
     player1.board = board1
